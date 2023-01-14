24843223decb7763adb69e6cd236493bba502062
biaska <biaska70@gmail.com>
2023-01-14 10:52:04 -0800

    Signup form with password validation fin

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -16,18 +16,34 @@ class Signup extends Component {
             confirmPassword: ''
         }
         
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleConPasswordChange = this.handleConPasswordChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    handlePasswordChange = (event) => {
+        this.setState({password: event.target.value})
+    }
+
+    handleConPasswordChange = (event) => {
+        this.setState({confirmPassword: event.target.value})
+    }
+
     handleSubmit = (event) => {
-
-        event.preventDefault()
-
-        if (event.target.value !== this.state.password) {
+        
+        event.preventDefault();
+        console.log(this.state.confirmPassword, this.state.password)
+        if (this.state.password !== this.state.confirmPassword) {
             alert('Passwords do not match');
             return false;
         } else {
-            return true;
+            if (this.state.password.length < 8) {
+                alert('Password must be 8 characters long');
+                return false;
+            } else {
+                fetch
+                return true;
+            }
         }
     }
 
@@ -37,18 +53,18 @@ class Signup extends Component {
                 <Row className='h-100'> 
                     <Col className='text-center align-items-center justify-content-center d-flex flex-column h-100'>
                         <img src={logo} alt='My HealthCare Logo' width={300}></img>
-                        <Form className='w-25' action='POST'>
+                        <Form className='w-25'>
                             <Form.Group className='mb-3' controlId='formUsername'>
                                 <Form.Label>Username</Form.Label>
-                                <Form.Control type='text' placeholder='Enter username' />
+                                <Form.Control type='text' placeholder='Enter username' autoComplete='username'/>
                             </Form.Group>
                             <Form.Group className='mb-3' controlId='formPassword'>
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control type='password' placeholder='Enter password'/>
+                                <Form.Control type='password' placeholder='Enter password' autoComplete='new-password' onChange={this.handlePasswordChange}/>
                             </Form.Group>
                             <Form.Group className='mb-3' controlId='formConfirmPassword'>
                                 <Form.Label>Re-Enter Password</Form.Label>
-                                <Form.Control type='password' placeholder='Re-enter password' />
+                                <Form.Control type='password' placeholder='Re-enter password' autoComplete='new-password' onChange={this.handleConPasswordChange}/>
                             </Form.Group>
                             <Button variant='info' type='submit' onClick={this.handleSubmit}>
                                 Submit
