5fa775d92d73fd0e4a0c1cebe49600cf14e9dfa6
biaska <biaska70@gmail.com>
2023-01-14 02:18:37 -0800

    Login page sketched

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,54 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
 import Login from './Login';
 import Signup from './Signup';
+import { Nav, Navbar, Container, Row, Col} from 'react-bootstrap';
+import logo from './logo_2.svg';
 
 /* vv Change to whatever we decide on this route vv */
 import Lookup from './Rxlookup';
 
+function NavBar(props) {
+  /* Routes that require the navbar are rendered through here first. Outlet (33) gives it a way to continue on to the correct path. This forces the 
+  navbar on the side, and every other view is rendered in the container on the right. */
+  return (
+    <div>
+      <Container className='h-100vh m-0 p-0' fluid>
+        <Row className='row-cols-2 h-100'>
+          <Col className='col-2 bg-dark text-center py-4'>
+              <Navbar className=' flex-column w-100 h-100 py-3'>
+                  <a href='/'>
+                    <img src={logo} alt='My HealthCare Logo' style={{width: 150, height: 75}}></img>
+                  </a>
+                    <Nav className='d-flex flex-column col-2 bg-dark text-center w-100 mh-100 gap-4'>
+                      <Nav.Link href='#' id='navlink' className='text-info fw-bolder my-5 fs-3'>Symptom <br /> Tracker</Nav.Link>
+                      <Nav.Link href='#' id='navlink' className='text-info fw-bolder my-5 fs-3'>Primary <br /> Care</Nav.Link>
+                      <Nav.Link href='#' id='navlink' className='text-info fw-bolder my-5 fs-3'>Notification <br /> Settings</Nav.Link>
+                      <Nav.Link href='#' id='navlink-white' className='text-white fw-bolder mt-5 fs-3'>Logout</Nav.Link>
+                    </Nav>
+              </Navbar>
+          </Col>
+          <Col className='col-10 p-0 m-0'>
+            <Outlet />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  )
+};
+
 /* All routes are extension components of the app component. App is running through BrowserRouter in index.js */
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: true,
+    }
+  }
+
+
   render() {
     return (
       <BrowserRouter>
@@ -18,9 +57,11 @@ class App extends Component {
           <Routes>
             {/* These routes assign an element to re-render that are imported from each JS view file */}
             <Route exact path='/' element={<Home />} />
-            <Route path='/Login' element={<Login />} />
-            <Route path='/RxLookup' element={<Lookup />} />
-            <Route path='/Signup' element={<Signup />} />
+            <Route element={<NavBar />}>
+              <Route path='/Login' element={<Login />} />
+              <Route path='/RxLookup' element={<Lookup />} />
+              <Route path='/Signup' element={<Signup />} />
+            </Route>
           </Routes>
         </div>
       </BrowserRouter>
--- client/src/Signup.js
+++ client/src/Signup.js
@@ -2,18 +2,58 @@ import './Signup.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Button, Form } from 'react-bootstrap';
 import { Component } from 'react';
 import logo from './logo_2.svg';
 
 
 
 class Signup extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            password: '',
+            confirmPassword: ''
+        }
+        
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit = (event) => {
+
+        event.preventDefault()
+
+        if (event.target.value !== this.state.password) {
+            alert('Passwords do not match');
+            return false;
+        } else {
+            return true;
+        }
+    }
+
     render() {
         return (
-            <Container className='window m-0 p-0' fluid>
-                <Row>
-                    <Col className='m-auto'>
-                        <img src={logo} alt="My Healthcare Logo"></img>
+            <Container className='window m-0 p-0 mh-100' fluid>
+                <Row className='h-100'> 
+                    <Col className='text-center align-items-center justify-content-center d-flex flex-column h-100'>
+                        <img src={logo} alt='My HealthCare Logo' width={300}></img>
+                        <Form className='w-25' action='POST'>
+                            <Form.Group className='mb-3' controlId='formUsername'>
+                                <Form.Label>Username</Form.Label>
+                                <Form.Control type='text' placeholder='Enter username' />
+                            </Form.Group>
+                            <Form.Group className='mb-3' controlId='formPassword'>
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type='password' placeholder='Enter password'/>
+                            </Form.Group>
+                            <Form.Group className='mb-3' controlId='formConfirmPassword'>
+                                <Form.Label>Re-Enter Password</Form.Label>
+                                <Form.Control type='password' placeholder='Re-enter password' />
+                            </Form.Group>
+                            <Button variant='info' type='submit' onClick={this.handleSubmit}>
+                                Submit
+                            </Button>
+                        </Form>
                     </Col>
                 </Row>
             </Container>
