351caf5e9027cc1bc86d4e50e696b1f5fad32521
biaska <biaska70@gmail.com>
2023-01-13 17:33:57 -0800

    Set up Router and landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Home';
+import Login from './Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* vv Change to whatever we decide on this route vv */
+import Lookup from './Rxlookup';
+
+/* All routes are an extension components of the app component. App is running through BrowserRouter in index.js */
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/Login' element={<Login />} />
+            <Route path='/RxLookup' element={<Lookup />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    )    
+
+  }
 }
 
 export default App;
