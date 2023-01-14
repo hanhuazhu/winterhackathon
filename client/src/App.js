6305ae36d041c670ebedf986453fea6efe44cff3
biaska <biaska70@gmail.com>
2023-01-13 17:56:04 -0800

    Added comments

--- client/src/App.js
+++ client/src/App.js
@@ -7,13 +7,15 @@ import Login from './Login';
 /* vv Change to whatever we decide on this route vv */
 import Lookup from './Rxlookup';
 
-/* All routes are an extension components of the app component. App is running through BrowserRouter in index.js */
+/* All routes are extension components of the app component. App is running through BrowserRouter in index.js */
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
+      {/* BrowserRouter catches the request from the browser and re-renders the page without requesting a new DOM */}
         <div>
           <Routes>
+            {/* These routes point assign an element to render. The elements are imported from each JS view file */}
             <Route exact path='/' element={<Home />} />
             <Route path='/Login' element={<Login />} />
             <Route path='/RxLookup' element={<Lookup />} />
@@ -21,7 +23,6 @@ class App extends Component {
         </div>
       </BrowserRouter>
     )    
-
   }
 }
 
